4f29224db016dcf6b93cb2a3737c94c042e6b7e7
Louthiern <louthiernbressan@live.fr>
2025-01-09 22:27:22 +0100

    New Configuration  to accomodate multiple files

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,22 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
+    const auto onClose = [&window](const sf::Event::Closed)
         {
-            if (event->is<sf::Event::Closed>())
+            window.close();
+        };
+    const auto onKeyPressed = [&window](const sf::Event::KeyPressed keyPressed)
+        {
+            if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
             {
                 window.close();
             }
-        }
+        };
 
+    while (window.isOpen())
+    {
+        window.handleEvents(onClose, onKeyPressed);
+        
         window.clear();
         window.display();
     }
